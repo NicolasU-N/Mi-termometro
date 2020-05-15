33c493bdf9fdc93570ecc1767a25a6f1491f4bc4
Nicolás Useche <nico311021@gmail.com>
2020-05-15 18:07:17 -0500

    Botones para iniciar/detener medición

--- lib/screens/HomeMain/userscreen.dart
+++ lib/screens/HomeMain/userscreen.dart
@@ -1,11 +1,14 @@
 import 'dart:async';
 import 'dart:convert' show utf8;
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'package:mi_termometro/screens/HomeMain/main_drawer.dart';
 
 import 'package:flutter_blue/flutter_blue.dart';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class UserScreen extends StatefulWidget {
   @override
@@ -13,6 +16,8 @@ class UserScreen extends StatefulWidget {
 }
 
 class _UserScreen extends State<UserScreen> with WidgetsBindingObserver {
+  final firestoreInstance = Firestore.instance;
+
   final SERVICE_UUID =
       "d39d2f80-94b3-11ea-bb37-0242ac130002"; // UART service UUID
   final CHARACTERISTIC_UUID = "d39d31e2-94b3-11ea-bb37-0242ac130002";
@@ -30,8 +35,10 @@ class _UserScreen extends State<UserScreen> with WidgetsBindingObserver {
 
   String connectionText = "";
   Stream<List<int>> stream;
-  List<double> _tempAmbl = List();
-  List<double> _tempCorl = List();
+  double _tempAmbl = 0;
+  double _tempCorl = 0;
+
+  bool estado = false;
 
   @override
   void initState() {
@@ -183,9 +190,24 @@ class _UserScreen extends State<UserScreen> with WidgetsBindingObserver {
 
                         if (snapshot.connectionState ==
                             ConnectionState.active) {
-                          var currentValue = _dataParser(snapshot.data);
-                          print(currentValue);
                           //traceDust.add(double.tryParse(currentValue) ?? "");
+
+                          var cutrrent_value = _dataParser(snapshot.data);
+
+                          if (estado) {
+                            _tempAmbl =
+                                double.parse(cutrrent_value.split(",")[0]);
+                            _tempCorl =
+                                double.parse(cutrrent_value.split(",")[1]);
+
+                            if (_tempCorl >= 36 && _tempCorl <= 37.5) {
+                              //print("ALERTA! Presencia de fiebre");
+                            }
+
+                            if (_tempCorl > 37.5 && _tempCorl <= 39) {
+                              // print("ALERTA! Presencia de fFEBRICULA");
+                            }
+                          }
 
                           return Center(
                               child: Column(
@@ -196,20 +218,63 @@ class _UserScreen extends State<UserScreen> with WidgetsBindingObserver {
                                 child: Column(
                                     mainAxisAlignment: MainAxisAlignment.center,
                                     children: <Widget>[
-                                      Text('Current value from Sensor',
-                                          style: TextStyle(fontSize: 14)),
+                                      MaterialButton(
+                                          splashColor:
+                                              Theme.of(context).primaryColor,
+                                          minWidth: 200,
+                                          height: 50,
+                                          color: Colors.green,
+                                          child: Text("Iniciar Medición ",
+                                              style: TextStyle(
+                                                  fontSize: 18,
+                                                  color: Colors.white)),
+                                          onPressed: () {
+                                            estado = true;
+                                          }),
+                                      Text("\n"),
                                       Text(
-                                          '\nTEMPERATURA AMBIENTE   || TEMPERATURA CORPORAL',
-                                          style: TextStyle(fontSize: 30)),
-                                      Text('${currentValue} °',
+                                          "TEMPERATURA AMBIENTE  ${_tempAmbl} °C",
+                                          style: TextStyle(fontSize: 20)),
+                                      Text(
+                                          "TEMPERATURA CORPORAL   ${_tempCorl} °C",
                                           style: TextStyle(
                                               fontWeight: FontWeight.bold,
-                                              fontSize: 24))
+                                              fontSize: 20)),
+                                      Text("\n"),
+                                      MaterialButton(
+                                        minWidth: 200,
+                                        height: 50,
+                                        color: Colors.red[400],
+                                        child: Text("Detener Medición ",
+                                            style: TextStyle(
+                                                fontSize: 18,
+                                                color: Colors.white)),
+                                        onPressed: () async {
+                                          estado = false;
+                                          /*
+                                          var firebaseUser = await FirebaseAuth
+                                              .instance
+                                              .currentUser();
+
+                                          firestoreInstance
+                                              .collection("users")
+                                              .document(firebaseUser.uid)
+                                              .updateData({
+                                            "age": 60,
+                                            "familyName": "Haddad",
+                                            "address.street": "street 50",
+                                            "address.country": "USA"
+                                          }).then((_) {
+                                            print("Success!");
+                                          }); */
+                                        },
+                                      ),
                                     ]),
                               ),
                             ],
                           ));
                         } else {
+                          //IMPORTANTE
                           return Center(
                             child: Column(
                               children: <Widget>[
